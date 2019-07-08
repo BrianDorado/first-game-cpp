48f6a8fcf5dec6580e42125c75661e78943a59c4
Brian Larson <manateeman24@hotmail.com>
2019-07-08 17:30:42 -0600

    changes to play game and guesses logic

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
+This project is a very simple game built in C++ 
+
 To compile:
 cl main.cpp
 
--- main.cpp
+++ main.cpp
@@ -1,24 +1,48 @@
 #include <iostream>
 
+void PrintIntro()
+{
+ std:: cout << "\nYou are a secret agent breaking into a secure server room\n You will need to enter the correct codes to continue\n\n";
+}
+
+void PlayGame()
+{
+  PrintIntro()
+
+  const int CodeA = 0;
+  const int CodeB = 1;
+  const int CodeC = 2;
+
+  const int sum = a+b+c;
+  const int product = a*b*c;
+
+  std::cout << "Each code will have three numbers\n";
+  std::cout << "The codes sum is: ";
+  std::cout << "\nThe codes product is: " << product <<std::endl;
+
+
+  int GuessA, GuessB, GuessC;
+  std::cin >> GuessA >> GuessB >> GuessC;
+  
+  int GuessSum = GuessA+GuessB+GuessC;
+  int GuessProd = GuessA*GuessB*GuessC;
+
+  if(GuessSum == sum && GuessProd == product) 
+  {
+    std::cout << "\nYou guessed correctly! ;D";
+  }
+  else {
+    std::cout << "\nYou guessed incorrectly! :'("
+  }
+}
+
 int main()
 {
-    std:: cout << "You are a secret agent breaking into a secure server room\n";
-    // std:: cout << std::endl
-
-    std::cout << "You need to enter the correct codes to continue";
-    /* code */
-
-    const int CodeA = 0;
-    const int CodeB = 1;
-    const int CodeC = 2;
-
-    const int sum = a+b+c;
-    const int product = a*b*c;
-    if (a != b)
-    {
-      std::cout << "Nothing happened";  /* code */
-    }
-    
-    
-    return a;
+  while()
+  {
+    PlayGame()
+    std::cin.clear();
+    std::cin.ignore();
+  }
+    return 0;
 }
